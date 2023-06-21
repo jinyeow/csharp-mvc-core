90120626edf9dc047db031dc2a9ac2a87bf1b85c
Justin Puah <justin@puah.dev>
2023-06-21 13:59:46 +0000

    Reset tutorial

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace csharp_mvc_core.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
